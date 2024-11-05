af0797a3f129ab2aa7452e66f6fd27d6bd10287a
gidon.bauer <gidon.bauer@hotmail.de>
2024-11-05 21:41:46 +0100

    Add option to use libfmt instead of stdlib

--- Igor/Logging.hpp
+++ Igor/Logging.hpp
@@ -22,12 +22,20 @@
 #ifndef IGOR_LOGGING_HPP_
 #define IGOR_LOGGING_HPP_
 
-#include <cstdint>
+#ifndef IGOR_USE_FMT
 #include <format>
+#else
+#include <fmt/chrono.h>
+#include <fmt/format.h>
+#include <fmt/ranges.h>
+#endif  // IGOR_USE_FMT
+
+#include <cstdint>
 #include <iostream>
 #include <ranges>
 #include <source_location>
 #include <string>
+#include <utility>
 
 namespace Igor {
 
@@ -40,6 +48,14 @@ enum class ExitCode : int {  // NOLINT(performance-enum-size)
 
 namespace detail {
 
+#ifndef IGOR_USE_FMT
+using std::format;
+using std::format_string;
+#else
+using fmt::format;
+using fmt::format_string;
+#endif  // IGOR_USE_FMT
+
 [[nodiscard]] constexpr auto strip_path(std::string_view full_path) noexcept -> std::string_view {
 #if defined(WIN32) || defined(_WIN32)
 #error "Not implemented yet: Requires different path separator ('\\') and potentially uses wchar"
@@ -49,9 +65,7 @@ namespace detail {
 
   size_t counter = 0;
   for (char c : std::ranges::reverse_view(full_path)) {
-    if (c == separator) {
-      break;
-    }
+    if (c == separator) { break; }
     ++counter;
   }
 
@@ -62,15 +76,15 @@ namespace detail {
 error_loc(const std::source_location loc = std::source_location::current()) noexcept
     -> std::string {
   try {
-    return std::format("`{}` (\033[95m{}:{}:{}\033[0m)",
-                       loc.function_name(),
+    return detail::format("`{}` (\033[95m{}:{}:{}\033[0m)",
+                          loc.function_name(),
 #ifdef IGOR_ERROR_LOC_FULL_PATH
-                       loc.file_name(),
+                          loc.file_name(),
 #else
-                       strip_path(loc.file_name()),
+                          strip_path(loc.file_name()),
 #endif  // IGOR_ERROR_LOC_FULL_PATH
-                       loc.line(),
-                       loc.column());
+                          loc.line(),
+                          loc.column());
   } catch (const std::exception& e) {
     std::cerr << "Could not format the error location: " << e.what() << '\n';
     std::exit(static_cast<int>(ExitCode::PANIC));
@@ -90,56 +104,43 @@ enum class Level : std::uint8_t {
 consteval auto level_stream(Level level) noexcept -> std::ostream& {
   switch (level) {
     case Level::INFO:
-    case Level::TIME:
-      return std::cout;
+    case Level::TIME:   return std::cout;
     case Level::WARN:
     case Level::TODO:
     case Level::PANIC:
     case Level::DEBUG:
-    case Level::ASSERT:
-      return std::cerr;
+    case Level::ASSERT: return std::cerr;
   }
 }
 
 consteval auto level_repr(Level level) noexcept {
   switch (level) {
-    case Level::INFO:
-      return "\033[32m[INFO]\033[0m ";
-    case Level::WARN:
-      return "\033[33m[WARN]\033[0m ";
-    case Level::TODO:
-      return "\033[35m[TODO]\033[0m ";
-    case Level::PANIC:
-      return "\033[31m[ERROR]\033[0m ";
-    case Level::DEBUG:
-      return "\033[94m[DEBUG]\033[0m ";
-    case Level::TIME:
-      return "\033[94m[TIME]\033[0m ";
-    case Level::ASSERT:
-      return "\033[31m[ASSERT]\033[0m ";
+    case Level::INFO:   return "\033[32m[INFO]\033[0m ";
+    case Level::WARN:   return "\033[33m[WARN]\033[0m ";
+    case Level::TODO:   return "\033[35m[TODO]\033[0m ";
+    case Level::PANIC:  return "\033[31m[ERROR]\033[0m ";
+    case Level::DEBUG:  return "\033[94m[DEBUG]\033[0m ";
+    case Level::TIME:   return "\033[94m[TIME]\033[0m ";
+    case Level::ASSERT: return "\033[31m[ASSERT]\033[0m ";
   }
 }
 
 template <Level level, ExitCode exit_code, typename... Args>
 class Print {
  protected:
-  constexpr Print(std::format_string<Args...> fmt, Args&&... args) noexcept {
+  constexpr Print(detail::format_string<Args...> fmt, Args&&... args) noexcept {
     auto& out = level_stream(level);
-    out << level_repr(level) << std::format(fmt, std::forward<Args>(args)...) << '\n';
-    if constexpr (exit_code != ExitCode::NO_EARLY_EXIT) {
-      std::exit(static_cast<int>(exit_code));
-    }
+    out << level_repr(level) << detail::format(fmt, std::forward<Args>(args)...) << '\n';
+    if constexpr (exit_code != ExitCode::NO_EARLY_EXIT) { std::exit(static_cast<int>(exit_code)); }
   }
 
   constexpr Print(const std::source_location loc,
-                  std::format_string<Args...> fmt,
+                  detail::format_string<Args...> fmt,
                   Args&&... args) noexcept {
     auto& out = level_stream(level);
     out << level_repr(level) << error_loc(loc) << ": "
-        << std::format(fmt, std::forward<Args>(args)...) << '\n';
-    if constexpr (exit_code != ExitCode::NO_EARLY_EXIT) {
-      std::exit(static_cast<int>(exit_code));
-    }
+        << detail::format(fmt, std::forward<Args>(args)...) << '\n';
+    if constexpr (exit_code != ExitCode::NO_EARLY_EXIT) { std::exit(static_cast<int>(exit_code)); }
   }
 };
 
@@ -150,11 +151,11 @@ class [[maybe_unused]] Time final
   using P = detail::Print<detail::Level::TIME, ExitCode::NO_EARLY_EXIT, Args...>;
 
  public:
-  constexpr Time(std::format_string<Args...> fmt, Args&&... args) noexcept
+  constexpr Time(detail::format_string<Args...> fmt, Args&&... args) noexcept
       : P{fmt, std::forward<Args>(args)...} {}
 };
 template <typename... Args>
-Time(std::format_string<Args...>, Args&&...) -> Time<Args...>;
+Time(detail::format_string<Args...>, Args&&...) -> Time<Args...>;
 
 }  // namespace detail
 
@@ -165,11 +166,11 @@ class [[maybe_unused]] Info final
   using P = detail::Print<detail::Level::INFO, ExitCode::NO_EARLY_EXIT, Args...>;
 
  public:
-  constexpr Info(std::format_string<Args...> fmt, Args&&... args) noexcept
+  constexpr Info(detail::format_string<Args...> fmt, Args&&... args) noexcept
       : P{fmt, std::forward<Args>(args)...} {}
 };
 template <typename... Args>
-Info(std::format_string<Args...>, Args&&...) -> Info<Args...>;
+Info(detail::format_string<Args...>, Args&&...) -> Info<Args...>;
 
 // -------------------------------------------------------------------------------------------------
 template <typename... Args>
@@ -178,13 +179,13 @@ class [[maybe_unused]] Warn final
   using P = detail::Print<detail::Level::WARN, ExitCode::NO_EARLY_EXIT, Args...>;
 
  public:
-  constexpr Warn(std::format_string<Args...> fmt,
+  constexpr Warn(detail::format_string<Args...> fmt,
                  Args&&... args,
                  const std::source_location loc = std::source_location::current()) noexcept
       : P{loc, fmt, std::forward<Args>(args)...} {}
 };
 template <typename... Args>
-Warn(std::format_string<Args...>, Args&&...) -> Warn<Args...>;
+Warn(detail::format_string<Args...>, Args&&...) -> Warn<Args...>;
 
 // -------------------------------------------------------------------------------------------------
 template <typename... Args>
@@ -198,7 +199,7 @@ class [[maybe_unused]] Todo final : detail::Print<detail::Level::TODO, ExitCode:
     std::unreachable();
   }
   [[noreturn]] constexpr Todo(
-      std::format_string<Args...> fmt,
+      detail::format_string<Args...> fmt,
       Args&&... args,
       const std::source_location loc = std::source_location::current()) noexcept
       : P{loc, fmt, std::forward<Args>(args)...} {
@@ -206,7 +207,7 @@ class [[maybe_unused]] Todo final : detail::Print<detail::Level::TODO, ExitCode:
   }
 };
 template <typename... Args>
-Todo(std::format_string<Args...>, Args&&...) -> Todo<Args...>;
+Todo(detail::format_string<Args...>, Args&&...) -> Todo<Args...>;
 
 // -------------------------------------------------------------------------------------------------
 template <typename... Args>
@@ -215,7 +216,7 @@ class [[maybe_unused]] Panic final : detail::Print<detail::Level::PANIC, ExitCod
 
  public:
   [[noreturn]] constexpr Panic(
-      std::format_string<Args...> fmt,
+      detail::format_string<Args...> fmt,
       Args&&... args,
       const std::source_location loc = std::source_location::current()) noexcept
       : P{loc, fmt, std::forward<Args>(args)...} {
@@ -223,7 +224,7 @@ class [[maybe_unused]] Panic final : detail::Print<detail::Level::PANIC, ExitCod
   }
 };
 template <typename... Args>
-Panic(std::format_string<Args...>, Args&&...) -> Panic<Args...>;
+Panic(detail::format_string<Args...>, Args&&...) -> Panic<Args...>;
 
 // -------------------------------------------------------------------------------------------------
 template <typename... Args>
@@ -233,7 +234,7 @@ class [[maybe_unused]] Assert final
 
  public:
   [[noreturn]] constexpr Assert(
-      std::format_string<Args...> fmt,
+      detail::format_string<Args...> fmt,
       Args&&... args,
       const std::source_location loc = std::source_location::current()) noexcept
       : P{loc, fmt, std::forward<Args>(args)...} {
@@ -241,20 +242,20 @@ class [[maybe_unused]] Assert final
   }
 };
 template <typename... Args>
-Assert(std::format_string<Args...>, Args&&...) -> Assert<Args...>;
+Assert(detail::format_string<Args...>, Args&&...) -> Assert<Args...>;
 
 #ifndef IGOR_NDEBUG
 // NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
 #define IGOR_ASSERT(cond, ...)                                                                     \
   do {                                                                                             \
     if (!(cond)) [[unlikely]] {                                                                    \
-      using t = decltype(std::make_tuple(__VA_ARGS__));                                            \
-      static_assert(std::tuple_size_v<t> > 0,                                                      \
+      using va_args_tuple__ = decltype(std::make_tuple(__VA_ARGS__));                              \
+      static_assert(std::tuple_size_v<va_args_tuple__> > 0,                                        \
                     "`IGOR_ASSERT` requires an error message, please provide a format string and " \
                     "the corresponding values");                                                   \
-      static_assert(std::is_convertible_v<std::tuple_element_t<0, t>, std::string>,                \
+      static_assert(std::is_convertible_v<std::tuple_element_t<0, va_args_tuple__>, std::string>,  \
                     "First argument for error message must be a format string.");                  \
-      Igor::Assert("Assertion `{}` failed: {}", #cond, std::format(__VA_ARGS__));                  \
+      Igor::Assert("Assertion `{}` failed: {}", #cond, Igor::detail::format(__VA_ARGS__));         \
     }                                                                                              \
   } while (false)
 #else
@@ -268,11 +269,11 @@ class [[maybe_unused]] Debug final
   using P = detail::Print<detail::Level::DEBUG, ExitCode::NO_EARLY_EXIT, Args...>;
 
  public:
-  constexpr Debug(std::format_string<Args...> fmt, Args&&... args) noexcept
+  constexpr Debug(detail::format_string<Args...> fmt, Args&&... args) noexcept
       : P{fmt, std::forward<Args>(args)...} {}
 };
 template <typename... Args>
-Debug(std::format_string<Args...>, Args&&...) -> Debug<Args...>;
+Debug(detail::format_string<Args...>, Args&&...) -> Debug<Args...>;
 
 #define IGOR_DEBUG_PRINT(x) Igor::Debug("{} = {}", #x, x)  // NOLINT(cppcoreguidelines-macro-usage)
 
--- Makefile
+++ Makefile
@@ -1,16 +1,24 @@
 TARGETS = ${basename ${wildcard *.cpp}}
+STD_TARGETS = ${addsuffix .std, ${TARGETS}}
+FMT_TARGETS = ${addsuffix .fmt, ${TARGETS}}
 
-CXX_FLAGS = -Wall -Wextra -pedantic -Wconversion -Wshadow -std=c++2b -O0 -g
+CXX_FLAGS = -Wall -Wextra -pedantic -Wconversion -Wshadow -std=c++23 -O0 -g -march=native
+FMT_FLAGS = -I/opt/homebrew/Cellar/fmt/11.0.2/include
+FMT_LIBS = -L/opt/homebrew/Cellar/fmt/11.0.2/lib -lfmt
 
 IGOR_FILES = Igor/Igor.hpp Igor/Logging.hpp Igor/Macros.hpp Igor/Math.hpp Igor/MemoryToString.hpp Igor/ProgressBar.hpp Igor/StaticVector.hpp Igor/Timer.hpp Igor/TypeName.hpp
 
-all: ${TARGETS}
+# all: ${STD_TARGETS} ${FMT_TARGETS}
+all: ${FMT_TARGETS}
 
 sanitize: CXX_FLAGS += -fsanitize=address,undefined,leak
 sanitize: ${TARGETS}
 
-%: %.cpp ${IGOR_FILES}
+%.std: %.cpp ${IGOR_FILES}
 	${CXX} ${CXX_FLAGS} -o $@ $<
 
+%.fmt: %.cpp ${IGOR_FILES}
+	${CXX} ${CXX_FLAGS} ${FMT_FLAGS} -DIGOR_USE_FMT -o $@ $< ${FMT_LIBS}
+
 clean:
-	rm -fr ${TARGETS} ${addsuffix .dSYM, ${TARGETS}}
+	rm -fr ${STD_TARGETS} ${FMT_TARGETS} ${addsuffix .dSYM, ${STD_TARGETS} ${FMT_TARGETS}}
