8766015bd2dea1c606559acfe0ebfb3c6cc9ae93
gidon.bauer <g.bauer@itv.rwth-aachen.de>
2025-11-12 10:40:47 +0100

    Use a static_cast instead of to_underlying

--- Igor/Logging.hpp
+++ Igor/Logging.hpp
@@ -58,7 +58,7 @@ enum class ExitCode : int {  // NOLINT(performance-enum-size)
   for (const auto& f : on_death) {
     f();
   }
-  if (exit_code == std::to_underlying(ExitCode::ASSERT)) { std::abort(); }
+  if (exit_code == static_cast<int>(ExitCode::ASSERT)) { std::abort(); }
   std::exit(exit_code);
 }
 
